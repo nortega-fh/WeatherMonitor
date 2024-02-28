c911a043c7e55ae0a946f06ebc8d9ca86c1dd9eb
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-02-28 17:01:50 -0500

    test(FileReader): implemented tests for file reader

--- WeatherMonitor.Tests/FileHandlers/FileReaderShould.cs
+++ WeatherMonitor.Tests/FileHandlers/FileReaderShould.cs
@@ -1,5 +1,43 @@
-﻿namespace WeatherMonitor.Tests.FileHandlers;
+﻿using AutoFixture;
+using Moq;
+using WeatherMonitor.Bots;
+using WeatherMonitor.Deserializers;
+using WeatherMonitor.FileHandlers;
+
+namespace WeatherMonitor.Tests.FileHandlers;
 
 public class FileReaderShould
 {
+    private static readonly Fixture Fixture = new();
+
+    [Fact]
+    public void ReadAnExistingFileAndDeserialize()
+    {
+        var path = Fixture.Create<string>();
+        var fileCreator = File.Create(path);
+        fileCreator.Close();
+        var expectedBot = Fixture.Create<SunBot>();
+        var deserializerMoq = new Mock<IDeserializer<IBot>>();
+        deserializerMoq.Setup(deserializer => deserializer.Deserialize(It.IsAny<Stream>()))
+            .Returns(expectedBot)
+            .Verifiable(Times.Once);
+        var sut = new FileReader<IBot>(deserializerMoq.Object);
+
+        var obtainedBot = sut.Read(path);
+
+        Assert.Equal(expectedBot, obtainedBot);
+
+        File.Delete(path);
+    }
+
+    [Fact]
+    public void ThrowWhenFileDoesNotExists()
+    {
+        var fakeFileName = Fixture.Create<string>();
+        var deserializerMoq = new Mock<IDeserializer<IBot>>();
+
+        var sut = new FileReader<IBot>(deserializerMoq.Object);
+
+        Assert.Throws<FileNotFoundException>(() => sut.Read(fakeFileName));
+    }
 }
