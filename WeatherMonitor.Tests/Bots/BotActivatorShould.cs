3c62d4aba71e6b7ddb220d4ce2fa5950a8ef2b63
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-02-28 21:12:25 -0500

    test(bots): bots test implemented

--- WeatherMonitor.Tests/Bots/BotActivatorShould.cs
+++ WeatherMonitor.Tests/Bots/BotActivatorShould.cs
@@ -1,5 +1,25 @@
-﻿namespace WeatherMonitor.Tests.Bots;
+﻿using AutoFixture;
+using Moq;
+using WeatherMonitor.Bots;
+using WeatherMonitor.Weather;
+
+namespace WeatherMonitor.Tests.Bots;
 
 public class BotActivatorShould
 {
+
+    [Fact]
+    public void ActivateBotsLoaded()
+    {
+        var fixture = new Fixture();
+        var weatherData = fixture.Create<WeatherData>();
+        var bot = new Mock<IBot>();
+        var listOfBots = new List<IBot>() { bot.Object };
+
+        var sut = new BotActivator(listOfBots);
+
+        sut.ActivateBots(weatherData);
+
+        bot.Verify(bot => bot.Activate(weatherData), Times.Once);
+    }
 }
---
+++ WeatherMonitor.Tests/Bots/EnabledAndInThresholdBotsAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using WeatherMonitor.Bots;
+using Xunit.Sdk;
+
+namespace WeatherMonitor.Tests.Bots;
+
+public class EnabledAndInThresholdBotsAttribute : DataAttribute
+{
+    public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+    {
+        yield return new object[] { new RainBot() { Enabled = true } };
+        yield return new object[] { new SunBot() { Enabled = true } };
+        yield return new object[] { new SnowBot() { Enabled = true, CentigradesTemperatureThreshold = 2 } };
+    }
+}
